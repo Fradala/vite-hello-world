a27af410c6d29668b792fc53e6176d737852c439
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-12-13 15:13:37 +0100

    esercizio svolto

---
+++ src/components/MyHelloWord.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    neme: 'MyHellowWord',
+    data() {
+        
+    },
+}
+</script>
+<template>
+    <h1>
+        La mia prima app con vite
+    </h1>
+</template>
+<style>
+</style>
